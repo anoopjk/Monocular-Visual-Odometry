14b1f54920b3426cd9978220d797f7816bb2a24d
anoopjk <anoopnwu@gmail.com>
2017-08-15 18:01:58 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,4 @@
-/*main file for processing the data and running the EKF*/
 
-
-// #include "features.h"
-// #include "EKF.h"
-
-
-#define MAX_FRAME 3000
-#define MIN_NUM_FEAT 300
 
 #include <algorithm> // for copy
 #include <iterator> // for ostream_iterator
@@ -27,42 +19,7 @@
 using namespace std;
 using namespace Eigen;
 using namespace cv;
-//The following function is based on the same from https://github.com/avisingh599/mono-vo
-double getAbsoluteScale(int frame_id)  {
-  
-  string line;
-  int i = 0;
-  ifstream myfile ("/home/anoop/Documents/robotics/EKF_mono_slam/KITTI01/dataset/poses/02.txt");
-  double x =0, y=0, z = 0;
-  double x_prev, y_prev, z_prev;
-  if (myfile.is_open())
-  {
-    while (( getline (myfile,line) ) && (i<=frame_id))
-    {
-      z_prev = z;
-      x_prev = x;
-      y_prev = y;
-      std::istringstream in(line);
-      //cout << line << '\n';
-      for (int j=0; j<12; j++)  {
-        in >> z ;
-        if (j==7) y=z;
-        if (j==3)  x=z;
-      }
-      
-      i++;
-    }
-    myfile.close();
-  }
 
-  else {
-    cout << "Unable to open file";
-    return 0;
-  }
-
-  return sqrt((x-x_prev)*(x-x_prev) + (y-y_prev)*(y-y_prev) + (z-z_prev)*(z-z_prev)) ;
-
-}
 int main( int argc, char** argv )
 {
   Mat currImage, prevImage;
@@ -72,14 +29,14 @@ int main( int argc, char** argv )
 
   double scale = 1.00;
 
-  String folderpath = "/home/anoop/Documents/yembo/processedIMG/*.png" ; //"/home/anoop/Documents/robotics/EKF_mono_slam/KITTI02/image_2/*.png";
+  String folderpath = "../IMG/*.png" ;
   vector<String> filenames;
   cv::glob(folderpath, filenames);
 
 
   Mat K(3, 3, CV_64F);
-  K.at<double>(0,0) = 1075.62   /*7.188560000000e+02*/ ;  K.at<double>(0,1) = 0.; K.at<double>(0,2) = 621.462 /*6.071928000000e+02 */;
-  K.at<double>(1,0) = 0.; K.at<double>(1,1) = 1075.62  /*7.188560000000e+02*/ ; K.at<double>(1,2) = 358.773 /*1.852157000000e+02 */;
+  K.at<double>(0,0) = 1075.62 ;  K.at<double>(0,1) = 0.; K.at<double>(0,2) = 621.462 ;
+  K.at<double>(1,0) = 0.; K.at<double>(1,1) = 1075.62 ; K.at<double>(1,2) = 358.773;
   K.at<double>(2,0) = 0.; K.at<double>(2,1) = 0.; K.at<double>(2,2) = 1.;
 
   Mat D(5, 1, CV_64F);
@@ -94,9 +51,9 @@ int main( int argc, char** argv )
   Mat R, t,tvec, E, mask;
   int features_count = 0;
 
-  cout << "before while loop" << endl;
+  //cout << "before while loop" << endl;
   cout << "no.of images: " << filenames.size() << endl;
-    while( true /*framenum <=3000*/)
+  for(int framenum = 0; framenum < int(filenames.size()); ++framenum)
     {
         cout << "Image#: " <<  framenum << endl;
         if (framenum >=0){
@@ -104,13 +61,9 @@ int main( int argc, char** argv )
             
             currImage = imread(filenames[framenum]);
             cout << filenames[framenum] << endl;
-            features_count = featureMatching(currImage, prevImage, K, R, t, tvec);
+            features_count = featureMatching(currImage, prevImage, K, R, t, tvec)
 
-            //scale
-            //scale = getAbsoluteScale(framenum ); 
-            //cout << "Scale is " << scale << endl;
-
-            if ( (!t.empty() && features_count > 5) /*&& (scale>0.1) &&(t.at<double>(2) > t.at<double>(0)) && (t.at<double>(2) > t.at<double>(1))*/) 
+            if ( (!t.empty() && features_count > 5) ) 
             {
                 //cout << "scale*t :" << scale*t << endl;   
                  t_f = t_f + scale*(R_f*t);
@@ -118,7 +71,6 @@ int main( int argc, char** argv )
             }
             
             else {
-           //cout << "scale below 0.1, or incorrect translation" << endl;
             }
             output << t_f.at<double>(0) << " " << t_f.at<double>(1) << " " << t_f.at<double>(2)  << endl;
              
@@ -128,8 +80,6 @@ int main( int argc, char** argv )
             waitKey(1);
 
         }
-    
-    framenum = framenum+1; // increment the image counter
 
     }                                  
    
